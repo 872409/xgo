7c0df06328aa9cb168e8c33135379ce2e3a2c1fe
x <872409@gmail.com>
2024-08-22 10:10:35 +0800

    Slice Shuffle

--- fp/group.go
+++ fp/group.go
@@ -1,5 +1,10 @@
 package fp
 
+import (
+	"math/rand"
+	"time"
+)
+
 func GroupBy[T any, K comparable](items []T, getProperty func(T) K) map[K][]T {
 	grouped := make(map[K][]T)
 
@@ -9,4 +14,15 @@ func GroupBy[T any, K comparable](items []T, getProperty func(T) K) map[K][]T {
 	}
 
 	return grouped
+}
+
+func SliceShuffle(a []any) {
+	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+}
+
+func SliceShuffleRand(r *rand.Rand, a []any) {
+	if r == nil {
+		r = rand.New(rand.NewSource(time.Now().Unix()))
+	}
+	r.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
 }
